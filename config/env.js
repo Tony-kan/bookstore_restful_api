4bc2316d5d5753a25e6b840ecc72260e48f67860
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-04-09 09:27:07 +0200

    changed dotenv

--- config/env.js
+++ config/env.js
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+// config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+// config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+config({ path: `.env` });
+
 
 export const { PORT, NODE_ENV, JWT_SECRET,JWT_EXPIRES_IN, DB_URI,LOG_LEVEL,CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET } =
   process.env;
